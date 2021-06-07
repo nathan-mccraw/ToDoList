3c3c975c23b44f02ee1a3448020d69c8e834fa88
nathan-mccraw <naterltw175@gmail.com>
2021-06-07 15:15:03 -0400

    Added prompt for user/task

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let userName = prompt("Enter your first name: ");
+while (!userName) userName = prompt("Enter your first name:");
 
+let activity = prompt("Enter the activity (ie grocery shopping, chores, etc):");
+
+document.getElementById("userName").textContent = userName + "'s";
+document.getElementById("activityType").textContent = activity;
