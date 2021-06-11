7b7190d213e46450d8986c05611750c95e00f566
nathan-mccraw <naterltw175@gmail.com>
2021-06-11 13:15:38 -0400

    Fixed delete button and completed task checkbox

--- script.js
+++ script.js
@@ -8,25 +8,10 @@
 // document.getElementById("userName").textContent = userName + "'s";
 // document.getElementById("activityType").textContent = activity;
 
-//Step 1. pass the data from the input field into a variable. todoInput.value;
-//Step 2. use createElement() to create <li></li>
-//Step 3. populate the textContent value of the li with the input value variable.
-//Step 4. use createElement() to create a delete button
-//Step 5. use appendChild() to append the delete button to the li
-//Step 6. use appendChild() to append the li to the todoList/ul
-
-//Function for form submission --if you hit submit
-//-text from the text box is added into an array
-//if statement
-//-if array is empty then display graphic else display array
-//array needs to display a check box on the left of the string and a button on the right of the string
-//-check box on left of string in array that does text-transform strikeout <- strikeout function?
-//-circular button on right with "x" that deletes array at that index <- delete function ?
-
 const addButton = document.getElementById("addButton");
 const addByTxt = document.getElementById("addByText");
 const clearButton = document.getElementById("clearList");
-let taskCounter = 1;
+const currentUL = document.getElementById("taskListUL");
 
 clearButton.addEventListener("click", () => {
   document.getElementById("taskListUL").innerHTML = "";
@@ -42,14 +27,11 @@ addByTxt.addEventListener("submit", (event) => {
 function addTask() {
   const newTask = document.createTextNode(todoInput.value);
   const newLI = document.createElement("li");
-  newLI.id = `taskText${taskCounter}`;
-  newLI.class = `task${taskCounter}`;
 
-  const currentUL = document.getElementById("taskListUL");
   newLI.appendChild(newTask);
 
   const newCheckBox = document.createElement("input");
-  createCheckBox(newCheckBox);
+  createCheckBox(newCheckBox, newLI);
 
   const newDeleteButton = document.createElement("button");
   createDeleteButton(newDeleteButton);
@@ -60,26 +42,18 @@ function addTask() {
 
   todoInput.value = "";
   todoInput.focus();
-  taskCounter++;
 }
 
 function createDeleteButton(newDeleteButton) {
   newDeleteButton.textContent = "x";
-  newDeleteButton.id = `deleteButton${taskCounter}`;
-  newDeleteButton.class = `task${taskCounter}`;
-  newDeleteButton.addEventListener("click", removeTask(taskCounter));
+  newDeleteButton.addEventListener("click", function () {
+    newDeleteButton.parentElement.style.display = "none";
+  });
 }
 
-function createCheckBox(newCheckBox) {
+function createCheckBox(newCheckBox, newLI) {
   newCheckBox.setAttribute("type", "checkbox");
-  newCheckBox.id = `checkBox${taskCounter}`;
-  newCheckBox.class = `task${taskCounter}`;
-  newCheckBox.addEventListener("click", strikeTask(taskCounter));
+  newCheckBox.addEventListener("click", function () {
+    newLI.classList.toggle("completedTask");
+  });
 }
-
-function removeTask(index) {
-  let taskToBeRemoved = document.querySelectorAll(`#task${index}`);
-  taskToBeRemoved.remove;
-}
-
-function strikeTask(index) {}
