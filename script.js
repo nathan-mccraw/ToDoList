8cf34de74463f3b06dd984cbca1fc5a2ba036523
nathan-mccraw <naterltw175@gmail.com>
2021-06-19 13:51:00 -0400

    Refactored JS

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
-const addButton = document.getElementById("addButton");
-const addByEnter = document.getElementById("addByText");
-const clearButton = document.getElementById("clearList");
-const UlNode = document.getElementById("taskListUL");
-
-clearButton.addEventListener("click", () => {
+document.getElementById("clearList").addEventListener("click", () => {
   document.getElementById("taskListUL").innerHTML = "";
 });
-addButton.addEventListener("click", addTask);
 
-addByEnter.addEventListener("submit", (event) => {
+document.getElementById("form").addEventListener("submit", (event) => {
   event.preventDefault();
   addTask();
 });
 
 function addTask() {
   if (todoInput.value === "") return;
-  const newTask = document.createTextNode(todoInput.value);
-  const newLI = document.createElement("li");
+  const taskList = document.getElementById("taskListUL");
+  const task = createTaskListItem(todoInput.value);
+  const newCheckBox = createCheckBox(task);
+  const newDeleteButton = createDeleteButton(task);
 
-  newLI.appendChild(newTask);
+  task.insertAdjacentElement("afterbegin", newCheckBox);
+  task.insertAdjacentElement("beforeend", newDeleteButton);
+  taskList.insertAdjacentElement("beforeend", task);
 
-  const newCheckBox = document.createElement("input");
-  createCheckBox(newCheckBox, newLI);
-
-  const newDeleteButton = document.createElement("button");
-  createDeleteButton(newDeleteButton);
-
-  newLI.insertAdjacentElement("afterbegin", newCheckBox);
-  newLI.insertAdjacentElement("beforeend", newDeleteButton);
-  UlNode.insertAdjacentElement("beforeend", newLI);
-
-  ClearInputField(todoInput);
+  ClearInputField();
 }
 
-function ClearInputField(todoInput) {
+function createTaskListItem(text) {
+  const newLI = document.createElement("li");
+  newLI.appendChild(document.createTextNode(text));
+  return newLI;
+}
+
+function ClearInputField() {
   todoInput.value = "";
   todoInput.focus();
 }
 
-function createDeleteButton(newDeleteButton) {
-  newDeleteButton.textContent = "x";
-  newDeleteButton.classList.add("DeleteButton");
-  newDeleteButton.addEventListener("click", function () {
-    newDeleteButton.parentElement.style.display = "none";
+function createDeleteButton(task) {
+  const button = document.createElement("button");
+  button.textContent = "x";
+  button.classList.add("DeleteButton");
+  button.addEventListener("click", () => {
+    task.remove();
   });
+  return button;
 }
 
-function createCheckBox(newCheckBox, newLI) {
+function createCheckBox(task) {
+  const newCheckBox = document.createElement("input");
   newCheckBox.setAttribute("type", "checkbox");
-  newCheckBox.addEventListener("click", function () {
-    newLI.classList.toggle("completedTask");
+  newCheckBox.addEventListener("click", () => {
+    task.classList.toggle("completedTask");
   });
+  return newCheckBox;
 }
